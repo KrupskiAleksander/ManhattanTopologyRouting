11e06170d6a9c9c91d72039060ae5fa61e80c6fd
szymonhulewicz <szhulewicz@gmail.com>
2018-12-30 18:27:34 +0100

    Generowanie scierwa

--- src/Elka/Soib/Network/EdgeWeightedDigraph.java
+++ src/Elka/Soib/Network/EdgeWeightedDigraph.java
@@ -1,16 +1,19 @@
 package Elka.Soib.Network;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EdgeWeightedDigraph {
     private final int V;
+    private final int squareWidth;
     private int E;
-    private Bag<DirectedEdge>[] adj;
+    private ArrayList<DirectedEdge> paths = new ArrayList<>();
+    //private Bag<DirectedEdge>[] adj;
 
-    public EdgeWeightedDigraph(int V) {
+    public EdgeWeightedDigraph(int V, int squareWidth) {
         this.V = V;
         this.E = E;
-        adj = (Bag<DirectedEdge>[]) new Bag[V];
-        for (int v = 0; v < V; v++)
-            adj[v] = new Bag<DirectedEdge>();
+        this.squareWidth = squareWidth;
     }
 
     public int V() {
@@ -21,19 +24,94 @@ public class EdgeWeightedDigraph {
         return E;
     }
 
-    public void addEdge(DirectedEdge e) {
-        adj[e.from()].add(e);
-    }
 
-    public Iterable<DirectedEdge>[] adj() {
-        return adj;
-    }
+    public void createSOIBGraphXd() {
+        Integer pathsCounter = 0;
+        Integer numberOfTop = 1;
+        Integer[][] tabOfTops = new Integer[squareWidth][squareWidth];
+        for(Integer i = 0; i < squareWidth; i++){
+            for(Integer j = 0; j < squareWidth; j++){
+                tabOfTops[i][j] = numberOfTop;
+                numberOfTop++;
+            }
+        }
 
-    public Iterable<DirectedEdge> edges() {
-        Bag<DirectedEdge> bag = new Bag<DirectedEdge>();
-        for (int v = 0; v < V; v++)
-            for (DirectedEdge e : adj[v])
-                bag.add(e);
-        return bag;
+        for(Integer i = 0; i < squareWidth; i++){
+            for(Integer j = 0; j < squareWidth; j++){
+
+                if(i%2 == 0 && (j+1) <= (squareWidth - 1)){
+                    Integer from = tabOfTops[i][j];
+                    Integer to = tabOfTops[i][j+1];
+                    DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                    paths.add(directedEdge);
+                    pathsCounter++;
+
+                    System.out.println("From: " + from + " To: " + to);
+                } else if ((j+1) <= (squareWidth - 1)) {
+                    Integer from = tabOfTops[i][j+1];
+                    Integer to = tabOfTops[i][j];
+                    DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                    paths.add(directedEdge);
+                    pathsCounter++;
+
+                    System.out.println("From: " + from + " To: " + to);
+                } else {
+                    if(i%2 == 0){
+                        Integer from = tabOfTops[i][squareWidth - 1];
+                        Integer to = tabOfTops[i][0];
+                        DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                        paths.add(directedEdge);
+                        pathsCounter++;
+
+                        System.out.println("From: " + from + " To: " + to);
+                    } else {
+                        Integer from = tabOfTops[i][0];
+                        Integer to = tabOfTops[i][squareWidth - 1];
+                        DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                        paths.add(directedEdge);
+                        pathsCounter++;
+
+                        System.out.println("From: " + from + " To: " + to);
+                    }
+                }
+
+                if(j%2 == 0 && (i + 1) <= (squareWidth - 1)){
+                    Integer from = tabOfTops[i+1][j];
+                    Integer to = tabOfTops[i][j];
+                    DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                    paths.add(directedEdge);
+                    pathsCounter++;
+
+                    System.out.println("From: " + from + " To: " + to);
+                } else if ((i + 1) <= (squareWidth - 1)) {
+                    Integer from = tabOfTops[i][j];
+                    Integer to = tabOfTops[i + 1][j];
+                    DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                    paths.add(directedEdge);
+                    pathsCounter++;
+
+                    System.out.println("From: " + from + " To: " + to);
+                } else {
+                    if(j%2 == 0){
+                        Integer from = tabOfTops[0][j];
+                        Integer to = tabOfTops[squareWidth - 1][j];
+                        DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                        paths.add(directedEdge);
+                        pathsCounter++;
+
+                        System.out.println("From: " + from + " To: " + to);
+                    } else {
+                        Integer from = tabOfTops[squareWidth - 1][j];
+                        Integer to = tabOfTops[0][j];
+                        DirectedEdge directedEdge = new DirectedEdge(from, to, 1);
+                        paths.add(directedEdge);
+                        pathsCounter++;
+
+                        System.out.println("From: " + from + " To: " + to);
+                    }
+                }
+            }
+        }
+        System.out.println("Liczba krawedzi: " + pathsCounter);
     }
 }
--- src/Elka/Soib/Network/Main.java
+++ src/Elka/Soib/Network/Main.java
@@ -3,6 +3,14 @@ package Elka.Soib.Network;
 public class Main {
 
     public static void main(String[] args) {
-        Digraph graph = Digraph.generateManhattanDigraph(4);
+
+        //Digraph graph = Digraph.generateManhattanDigraph(4);
+        Integer squareWidth = 4;
+        Integer numberOfGraphTops = squareWidth*squareWidth;
+
+        EdgeWeightedDigraph edgeWeightedDigraph = new EdgeWeightedDigraph(numberOfGraphTops, squareWidth);
+
+        edgeWeightedDigraph.createSOIBGraphXd();
+
     }
 }
