e356e5a468c482347071428a3f122dcf938e63c9
Aleksander Krupski <aleksanderkrupski@gmail.com>
2019-01-20 13:27:46 +0100

    Działa WTF

--- src/Elka/Soib/Network/Main.java
+++ src/Elka/Soib/Network/Main.java
@@ -2,6 +2,7 @@ package Elka.Soib.Network;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -9,6 +10,8 @@ public class Main {
 
         //Digraph graph = Digraph.generateManhattanDigraph(4);
         Integer squareWidth =15;
+        int packetRatio = 20;
+        int timeOfSimulation = 100;
         Integer numberOfGraphTops = squareWidth*squareWidth;
 
         EdgeWeightedDigraph edgeWeightedDigraph = new EdgeWeightedDigraph(numberOfGraphTops, squareWidth);
@@ -20,26 +23,59 @@ public class Main {
         floydWarshall.findShortestPaths(edgeWeightedDigraph);
 
 //        Packet packet = new Packet(23,22);
-          Packet.timeToLeave=3;
+          Packet.timeToLeave=20;
+          Packet.buffers = new Buffer(squareWidth * squareWidth);
+          Buffer.bufferSize = 20;
 //        packet.generateRoute(floydWarshall);
+
+
+
+
+
         List<Packet> pakiety =  new ArrayList<>();
-        for (int i = 0; i <200; i++){
-            pakiety.add(new Packet(i+1,i+2));
-            pakiety.get(i).generateRoute(floydWarshall);
-            pakiety.get(i).buffers = new Buffer(squareWidth * squareWidth);
-        }
+        for (int i = 0; i <timeOfSimulation; i++){
+            for (int j = 0; j <packetRatio; j++) {
+                int random1 = ThreadLocalRandom.current().nextInt(0, (squareWidth * squareWidth));
+                int random2 = ThreadLocalRandom.current().nextInt(0, (squareWidth * squareWidth));
+                while  (random2 == random1){
+                    random2 = ThreadLocalRandom.current().nextInt(0, (squareWidth * squareWidth));
+                }
+                Packet pakiecik = new Packet(random1, random2);
 
-        Buffer.bufferSize = 1;
+                pakiecik.generateRoute(floydWarshall);
+                pakiecik.send();
+                pakiety.add(pakiecik);
 
-        for(Packet pakiet : pakiety){
-            pakiet.send();
-        }
 
-        for (int i = 0 ; i< 200; i++ ){
+            }
             for(Packet pakiet : pakiety){
                 pakiet.traverse();
             }
+
         }
+
+
+//        for (int i = 0; i <200; i++){
+//            pakiety.add(new Packet(i+1,i+2));
+//            pakiety.get(i).generateRoute(floydWarshall);
+//        }
+//
+//        Buffer.bufferSize = 1;
+//
+//        for(Packet pakiet : pakiety){
+//            pakiet.send();
+//        }
+//
+//        for (int i = 0 ; i< 200; i++ ){
+//            for(Packet pakiet : pakiety){
+//                pakiet.traverse();
+//            }
+//            pakiety.add(new Packet(i+1,i+2));
+//            pakiety.get(pakiety.size() - 1).generateRoute(floydWarshall);
+//            pakiety.get(pakiety.size() - 1).buffers = new Buffer(squareWidth * squareWidth);
+//            pakiety.get(pakiety.size() - 1).send();
+//
+//        }
         int a =1;
         int b = 0;
         int c = 0;
